647f8cc06b43fcea16624bd2ce8ace97aa38470c
vivek bhardwaj <vivekb153@gmail.com>
2024-12-19 22:18:52 -0500

    add fixes for application and orders controllers

--- backend/app/controllers/orders_controller.rb
+++ backend/app/controllers/orders_controller.rb
@@ -4,7 +4,6 @@ class OrdersController < ApplicationController
     render json: @orders
   end
 
-  # POST /orders
   def create
     @order = Order.new(order_params)
 
@@ -17,7 +16,6 @@ class OrdersController < ApplicationController
 
   private
 
-  # Strong Parameters
   def order_params
     params.require(:order).permit(:user_id, :total_price, :status)
   end
